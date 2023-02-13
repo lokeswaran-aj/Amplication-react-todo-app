4f6e1212061a74512d14b0373692b35ed91f6220
Lokeswaran Aruljothi <74011196+a-loke@users.noreply.github.com>
2023-02-13 21:30:12 +0530

    first commit

---
+++ web/src/Tasks.js
@@ -0,0 +1,15 @@
+import Task from "./Task";
+
+export default function Tasks({ tasks, toggleCompleted }) {
+    return (
+        <ul>
+            {tasks.map((task) => (
+                <Task
+                    key={task.id}
+                    task={task}
+                    toggleCompleted={toggleCompleted}
+                />
+            ))}
+        </ul>
+    );
+}
